c8a4035176d82fc7530007e05af6bfc188357bc2
Simon Elbrink <simon.elbrink@lexicon.se>
2020-11-05 13:48:15 +0100

    Today's Workshop. Add, nameExist, Remove & Clear.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,8 +6,93 @@ package se.lexicon;
  */
 public class App 
 {
+
+   static String[] names = new String[] {};
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        add("Simon Elbrink");
+        add("Ulf Bengtsson");
+        add("Erik Svensson");
+
+        System.out.println(Arrays.toString(names));
+
+        remove("Ulf Bengtsson");
+        System.out.println(Arrays.toString(names));
+
+
     }
+
+    public static boolean add(String fullName){
+
+        boolean isAdded = false;
+
+        if (!nameExists(fullName)){
+            names = Arrays.copyOf(names, names.length +1);
+            names[names.length -1] = fullName;
+            isAdded = true;
+        }
+
+        return isAdded;
+    }
+
+    public static boolean nameExists(String fullName){
+        boolean result = false;
+
+        for (int i = 0; i < App.names.length; i++) {
+            if (App.names[i].equals(fullName)){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     *
+     * @param fullName
+     * @return
+     */
+    public static boolean remove(String fullName){
+        boolean isDeleted = false;
+
+        if (names.length == 0){
+            System.out.println("Array is empty");
+
+        }else if(nameExists(fullName)){
+
+            String[] newArray = new String[names.length -1];
+
+            for (int i = 0, j = 0 ; i < names.length; i++, j++){
+                if (names[i].equals(fullName)){
+                    --j;
+                    isDeleted = true;
+                }else{
+                    newArray[j] = names[i];
+                }
+
+            }
+
+            names = newArray;
+
+            return isDeleted;
+
+        }
+
+        return isDeleted;
+    }
+
+
+    public static void clear(){
+        if (App.names == null){
+            System.out.println("Name list is not initialized, but now created.");
+        }else{
+            System.out.println("Name list had been emptied!");
+        }
+
+        App.names = new String[0];
+    }
+
+
 }
--- src/test/java/se/lexicon/AppTest.java
+++ src/test/java/se/lexicon/AppTest.java
@@ -1,8 +1,9 @@
 package se.lexicon;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
@@ -17,4 +18,44 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
+    @Test
+    public void add_should_success_when_fullName_notExist(){
+
+        // Arrange
+
+        App.names = new String[0];
+
+        boolean expected = true;
+        boolean actual = false;
+        String fullName = "Simon Elbrink";
+
+
+        // Act
+        actual = App.add(fullName);
+
+        // Assert
+        assertEquals(expected, actual);
+        assertTrue(actual);
+        assertTrue(App.names.length == 1);
+        assertEquals(App.names[0], fullName);
+    }
+
+    @Test
+    public void add_should_return_false_when_fullName_exists(){
+
+        //Arrange
+        String fullName = "Simon Elbrink";
+
+        App.names = new String[] {fullName};
+
+        //Act
+       boolean actual = App.add(fullName);
+
+        //Assert
+        assertFalse(actual);
+
+    }
+
 }
